93fb55e7334494c6cc1f40ec0cc2c4372e215c8c
sfslime <93108188+sfslime@users.noreply.github.com>
2023-06-01 16:06:25 +0900

    プレイヤーを発見した際に追跡をしてくる処理

--- PliesonBreak/Assets/Jailer.cs
+++ PliesonBreak/Assets/Jailer.cs
@@ -6,20 +6,28 @@ using UnityEngine.UI;
 public class Jailer : MonoBehaviour
 {
     NavMeshAgent2D NavMeshAgent2D; //NavMeshAgent2Dを使用するための変数
-    //[SerializeField] Transform Target; //追跡するターゲット
+    [SerializeField] Transform Target; //追跡するターゲット
 
     [SerializeField] List<Transform> PatrolPointList = new List<Transform>();
     int PatrolNumIndex;
 
+    [SerializeField] bool isDiscover; // プレイヤーを見つけているかどうか.
+
     void Start()
     {
         NavMeshAgent2D = GetComponent<NavMeshAgent2D>(); //agentにNavMeshAgent2Dを取得
-        // SetNextPatrolPoint();
+        isDiscover = false;
     }
 
     void Update()
     {
         SetNextPatrolPoint();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        JailerSight();
     }
 
     /// <summary>
@@ -27,11 +35,53 @@ public class Jailer : MonoBehaviour
     /// </summary>
     void SetNextPatrolPoint()
     {
-        if(NavMeshAgent2D.isArrival == true)
+        if(isDiscover == false)
         {
-            PatrolNumIndex = (PatrolNumIndex + 1) % PatrolPointList.Count;
-            NavMeshAgent2D.isArrival = false;
+            if (NavMeshAgent2D.isArrival == true)
+            {
+                PatrolNumIndex = (PatrolNumIndex + 1) % PatrolPointList.Count;
+                NavMeshAgent2D.isArrival = false;
+            }
+            NavMeshAgent2D.SetDestination(PatrolPointList[PatrolNumIndex].position);
         }
-        NavMeshAgent2D.SetDestination(PatrolPointList[PatrolNumIndex].position);
+        else if (isDiscover == true)
+        {
+            // プレイヤーを追いかける処理.
+            NavMeshAgent2D.SetDestination(Target.position);
+        }
+        
+    }
+
+    /// <summary>
+    /// 敵の視界 扇形
+    /// </summary>
+    void JailerSight()
+    {
+        int   RayNum = 5;     // Rayの本数.
+        float Angle = 60f;    // 角度.
+        float Distance = 10f; // Rayの距離.
+        float StartAngle = -Angle / 2;     // 扇の開始角度.
+        float AngleIncrement = Angle / (RayNum - 1); // Rayの角度増分
+        Vector2 Velocity = GetComponent<Rigidbody2D>().velocity; // オブジェクトの速度を取得
+        Vector2 ForwardDir = Velocity.normalized; // オブジェクトが向いている方向の取得.
+
+        for (int num = 0; num < RayNum; num++)
+        {
+            // 現在の向き.
+            float CurrentAngle = StartAngle + num * AngleIncrement;
+
+            Quaternion RayRotation = Quaternion.Euler(0, 0, CurrentAngle);
+            Vector2 RayDir = RayRotation * ForwardDir;
+
+            // Rayの発射.
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, RayDir, Distance);
+
+            if (hit.collider != null && hit.collider.CompareTag("Player"))
+            {
+                isDiscover = true;
+            }
+
+            Debug.DrawRay(transform.position, RayDir * Distance, Color.red);
+        }
     }
 }

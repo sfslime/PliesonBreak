d5ca1e87dfa3c578eb415a92cbccb8474355be0f
sfslime <93108188+sfslime@users.noreply.github.com>
2023-06-08 09:31:02 +0900

    看守のパラメータをインスペクターで編集できるように変更

--- PliesonBreak/Assets/Scripts/Online/ConectServer.cs
+++ PliesonBreak/Assets/Scripts/Online/ConectServer.cs
@@ -46,7 +46,7 @@ public class ConectServer : MonoBehaviourPunCallbacks
     public void TryRoomJoin()
     {
         //ルームに参加する（ルームが存在しなければ作成して参加する）
-        PhotonNetwork.JoinOrCreateRoom(RoomProperties.RoomName, new RoomOptions(), TypedLobby.Default);
+        PhotonNetwork.JoinOrCreateRoom("1", new RoomOptions(), TypedLobby.Default);
     }
 
     // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
